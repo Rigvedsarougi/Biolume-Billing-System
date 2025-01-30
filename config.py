dfa28bf976e25ba1b1edb639db8dea3f4b9643e6
rigved-sarougi <dataanalyst@biolume.in>
2025-01-30 13:47:56 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+# Company Details
+COMPANY_NAME = "KS Agencies"
+COMPANY_ADDRESS = """61A/42, Karunanidhi Street, Nehru Nagar,
+West Velachery, Chennai - 600042.
+GSTIN/UIN: 33AAGFK1394P1ZX
+State Name : Tamil Nadu, Code : 33
+"""
+
+COMPANY_LOGO = 'Untitled design (3).png'
+PHOTO_LOGO = '10.png'
+
+BANK_DETAILS = """
+For Rtgs / KS Agencies
+Kotak Mahindra Bank
+Velachery branch
+Ac No 0012490288
+IFSC code KKBK0000473
+Mobile - 9444454461 / GPay / PhonePe / Niyas
+"""
