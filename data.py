571a61feb193eb8ae8fbb5290add8451f7b4b1d4
rigved-sarougi <dataanalyst@biolume.in>
2025-01-31 14:01:00 +0530

    Update data.py

--- data.py
+++ data.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 # Load product data and Party data
 biolume_df = pd.read_csv('MKT+Biolume - Inventory System - Invoice (2).csv')
-party_df = pd.read_csv('MKT+Biolume - Inventory System - Party.csv')
+party_df = pd.read_csv('MKT+Biolume - Inventory System - Party (2).csv')
 
 # Company Details
 company_name = "KS Agencies"
